52bf8735b93b8818e366ddea959b10bb239a8054
manoj <watermark0913@gmail.com>
2017-03-30 18:14:48 +0530

    save

---
+++ googleCaptcha.php
@@ -0,0 +1,11 @@
+<html>
+  <head>
+      <title></title>
+	  <script src='https://www.google.com/recaptcha/api.js'></script>
+  </head>
+  <body>
+    <form>
+	 <div class="g-recaptcha" data-sitekey="6LeH7SgTAAAAADQfAKISW_LyQdwkNG6f7DQN2iLO"></div>
+	</form>
+  </body>
+</html>
